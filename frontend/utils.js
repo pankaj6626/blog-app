35881f910370b57c296dbe631ad2a9db08cfecf2
pankaj awasthi <pankajawasthi9569@gmail.com>
2026-08-29 15:04:34 +0530

    select url based on env

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,3 +1,9 @@
+const isLocalEnvironment =
+  typeof window !== "undefined" &&
+  (window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1");
+
 export const BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+  import.meta.env.VITE_BACKEND_URL ||
+  (isLocalEnvironment ? "http://localhost:4000" : "https://blog-app-9ifo.onrender.com");
 //..
